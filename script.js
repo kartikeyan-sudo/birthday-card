53a792d1b0998b1285b008842c399cc3773765f6
Kartikeyan Dubey <76141663+kartikeyan-sudo@users.noreply.github.com>
2025-05-26 12:54:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ document.getElementById('cutCakeBtn').onclick = function() {
     nextSlide();
     // Autoplay video
     // ---- PUT YOUR GOOGLE DRIVE FILE ID BELOW ----
-    document.getElementById('birthdayVideo').src = "https://drive.google.com/file/d/1oLQc2fEMiM48KVD2DTImeQE2kfNf7BDv/preview";
+    document.getElementById('birthdayVideo').src = "birthday-card/VID-20250526-WA0006.mp4";
     // Link added as requested, no extra edits:
     // https://drive.google.com/file/d/1oLQc2fEMiM48KVD2DTImeQE2kfNf7BDv/view?usp=drivesdk
   }, 1100);
